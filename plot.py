fc717b85ee0c213dc86b981c39b0d9ea1eab246c
Wei-Chung Liao <wchngliao@gmail.com>
2018-08-22 20:05:22 +0800

    Add plot

---
+++ plot.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+
+import json
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data', type=str, nargs='+')
+    parser.add_argument('--name', type=str, nargs='*', default=None)
+    parser.add_argument('--title', type=str, default='Training Curve')
+    parser.add_argument('--xlabel', type=str, default='Epochs')
+    parser.add_argument('--ylabel', type=str, default='Accuracy')
+    parser.add_argument('--figure_num', type=int, default=None)
+
+    parser.add_argument('--save', type=bool, default=True)
+    parser.add_argument('--display', type=bool, default=True)
+
+    parser.add_argument('--filename', type=str, default='figure.png')
+
+    return parser.parse_args()
+
+
+def plot_figure(y, name=None, title=None, xlabel=None, ylabel=None, figure_num=None, display=True, save=False, filename='figure.png'):
+    if figure_num is None:
+        plt.figure()
+    else:
+        plt.figure(figure_num)
+
+    plt.clf()
+
+    if title is not None:
+        plt.title(title)
+    if xlabel is not None:
+        plt.xlabel(xlabel)
+    if ylabel is not None:
+        plt.ylabel(ylabel)
+
+    if name is None:
+        for y_ in y:
+            plt.plot(y_)
+    else:
+        for y_, name_ in zip(y, name):
+            plt.plot(y_, label=name_)
+        plt.legend(loc='lower right')
+
+    if save:
+        plt.savefig(filename)
+    if display:
+        plt.show()
+
+
+def main():
+    args = parse_args()
+
+    y = []
+    for data in args.data:
+        with open(data, 'r') as f:
+            y.append(json.load(f))
+
+    plot_figure(y=y,
+                name=args.name,
+                title=args.title,
+                xlabel=args.xlabel,
+                ylabel=args.ylabel,
+                figure_num=args.figure_num,
+                display=args.display,
+                save=args.save,
+                filename='figure.png'
+                )
+
+
+if __name__ == '__main__':
+    main()
